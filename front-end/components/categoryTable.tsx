87b8dc85f95bad4d3a4493a0fc83022916ce0972
mstars_lab3_03 <mstars_lab3_03@mstars-lab3-03s-iMac.local>
2022-08-05 12:59:29 +0800

    ADD category working

--- front-end/components/categoryTable.tsx
+++ front-end/components/categoryTable.tsx
@@ -11,60 +11,75 @@ import Button from "@mui/material/Button";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { useRouter } from "next/router";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
 
 export default function BasicTable({ categories }: any) {
   const router = useRouter();
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Category</TableCell>
-            <TableCell align="right">Index</TableCell>
-            <TableCell align="right">Color</TableCell>
-            <TableCell align="right"></TableCell>
-            <TableCell align="right"></TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {categories?.map(
-            (category: { _id: number; name: string; color: string }) => (
-              <TableRow
-                key={category._id}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {category.name}
-                </TableCell>
-                <TableCell align="right">{category._id}</TableCell>
-                <TableCell align="right">{category.color}</TableCell>
-                <TableCell align="right">
-                  <Button
-                    href={`http://localhost:3000/category/edit/${category._id}`}
-                    variant="outlined"
-                  >
-                    {<EditIcon />}
-                  </Button>
-                </TableCell>
-                <TableCell align="left">
-                  <Button
-                    variant="contained"
-                    color="error"
-                    onClick={() => {
-                      axios.delete("http://18.141.207.7:3002/category", {
-                        data: category,
-                      });
-                      router.push("/category");
-                    }}
-                  >
-                    {<DeleteIcon />}
-                  </Button>
-                </TableCell>
-              </TableRow>
-            )
-          )}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <div>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Category</TableCell>
+              <TableCell align="right">Index</TableCell>
+              <TableCell align="right">Color</TableCell>
+              <TableCell align="right"></TableCell>
+              <TableCell align="right"></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {categories?.map(
+              (category: { _id: number; name: string; color: string }) => (
+                <TableRow
+                  key={category._id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {category.name}
+                  </TableCell>
+                  <TableCell align="right">{category._id}</TableCell>
+                  <TableCell align="right">{category.color}</TableCell>
+                  <TableCell align="right">
+                    <Button
+                      href={`http://localhost:3000/category/edit/${category._id}`}
+                      variant="outlined"
+                    >
+                      {<EditIcon />}
+                    </Button>
+                  </TableCell>
+                  <TableCell align="left">
+                    <Button
+                      variant="contained"
+                      color="error"
+                      onClick={() => {
+                        axios.delete("http://18.141.207.7:3002/category", {
+                          data: category,
+                        });
+                        router.push("/category");
+                      }}
+                    >
+                      {<DeleteIcon />}
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              )
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <div style={{ textAlign: "center" }}>
+        <Button
+          variant="contained"
+          color="success"
+          style={{ width: "20vw" }}
+          onClick={() => {
+            router.push("/category/add");
+          }}
+        >
+          <AddCircleIcon /> Add Food
+        </Button>
+      </div>
+    </div>
   );
 }
---
+++ front-end/pages/category/add.jsx
@@ -0,0 +1,94 @@
+import axios from "axios";
+import * as React from "react";
+import {
+  Box,
+  TextField,
+  Select,
+  MenuItem,
+  InputLabel,
+  Button,
+  Stack,
+  Snackbar,
+  Alert,
+} from "@mui/material";
+import { useRouter } from "next/router";
+
+export default function add() {
+  const [open, setOpen] = React.useState(false);
+  const router = useRouter();
+  function submitHandler(e) {
+    e.preventDefault();
+    axios.post("http://18.141.207.7:3002/category", {
+      name: e.target.name.value,
+      color: e.target.color.value,
+    });
+    router.push("/category");
+    setOpen(true);
+  }
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <Button href="/food" variant="contained" color="success">
+        Back
+      </Button>
+      <h1>Add Food</h1>
+      <Stack sx={{ width: "100%" }} spacing={2}>
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+          <Alert
+            onClose={handleClose}
+            severity="success"
+            sx={{ width: "100%" }}
+          >
+            Амжилттай хадгалагдлаа!
+          </Alert>
+        </Snackbar>
+      </Stack>
+      <Box
+        component="form"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "15px",
+          "& .MuiTextField-root": { width: "50ch" },
+        }}
+        spacing={2}
+        noValidate
+        autoComplete="off"
+        onSubmit={submitHandler}
+      >
+        <TextField
+          label="Name"
+          variant="standard"
+          color="success"
+          name="name"
+          focused
+        />
+        <TextField
+          label="Color"
+          variant="standard"
+          color="success"
+          name="color"
+          focused
+        />
+
+        <Button variant="contained" color="success" type="submit">
+          Хадгалах
+        </Button>
+      </Box>
+    </div>
+  );
+}
